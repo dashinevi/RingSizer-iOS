45ec68775e2a79b9588f08d3db4589e6a0bfafda
Diana Dashynevich <ddashinevich@gmail.com>
2025-03-11 14:11:36 +0100

    Initial Commit

---
+++ RingSizer/RingSizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingSizerApp.swift
+//  RingSizer
+//
+//  Created by Diana Dashinevich on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RingSizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
